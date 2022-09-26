97c09b7c5b532bc81470dc396612a90b3550a8bf
nydina <63366733+nydina@users.noreply.github.com>
2022-09-26 13:57:17 +0200

    zoom mapRegion

--- PlacesOnMap.swift
+++ PlacesOnMap.swift
@@ -8,7 +8,7 @@ import MapKit
 import SwiftUI
 
 struct PlacesOnMap: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 48.85571241521854, longitude: 2.351696387064438), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 48.85571241521854, longitude: 2.351696387064438), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
     var body: some View {
         NavigationView {
             Map(coordinateRegion: $mapRegion, annotationItems: places){ place in

a310b6cf566b19ead81e826063238b1517c164bb
nydina <63366733+nydina@users.noreply.github.com>
2022-09-26 09:51:03 +0200

    Initial Commit

---
+++ CoworkingMap/CoworkingMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoworkingMapApp.swift
+//  CoworkingMap
+//
+//  Created by Dina Andrianarijaona on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoworkingMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

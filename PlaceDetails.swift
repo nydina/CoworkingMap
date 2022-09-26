6bd0f9834f213efd10eb40f91dbbe16849b5cb28
nydina <63366733+nydina@users.noreply.github.com>
2022-09-26 16:22:02 +0200

    Cut text description

--- PlaceDetails.swift
+++ PlaceDetails.swift
@@ -19,7 +19,7 @@ struct PlaceDetails: View {
                 .font(.title)
             Text(place.address)
                 .font(.headline)
-            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ")
                 .frame(width: UIScreen.main.bounds.width * 0.95, height: UIScreen.main.bounds.height * 0.25)
             
             if !place.url.isEmpty {

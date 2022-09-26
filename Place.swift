59902b33952147028810f3a5185e981d46987ed7
nydina <63366733+nydina@users.noreply.github.com>
2022-09-26 10:48:16 +0200

    create func load to parse json

--- Place.swift
+++ Place.swift
@@ -17,10 +17,31 @@ struct Place: Identifiable, Decodable {
     var wifi: Bool
     
     enum PlaceTypeEnum: String, Decodable {
-        case coffee, coworking, coffeeCoworking = "Coffee / Coworking"
+        case coffee, coworking, coffeeCoworking = "coffee / coworking"
         
     }
 }
+
+func load<T: Decodable>(_ filename: String) -> T {
+let data: Data
+guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+else {
+fatalError("Couldn't find \(filename) in main bundle.")
+}
+do {
+data = try Data(contentsOf: file)
+} catch {
+fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+}
+do {
+let decoder = JSONDecoder()
+return try decoder.decode(T.self, from: data)
+} catch {
+fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+}
+}
+var places: [Place] = load("places.json")
+
 
 
 
